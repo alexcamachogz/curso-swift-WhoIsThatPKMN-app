219771a90aa39aa7d3685c8eec84511e21b355f4
Alex Camacho <camacho.ale94@gmail.com>
2022-08-11 13:27:16 -0500

    Clase 24: Navegación entre vistas

--- who is that pokemon/Controller/ResultsViewController.swift
+++ who is that pokemon/Controller/ResultsViewController.swift
@@ -6,16 +6,32 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var pokemonLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var pokemonName: String = ""
+    var pokemonImageURL: String = ""
+    var finalScore: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scoreLabel.text = "Perdiste, tu puntaje fue de \(finalScore)."
+        pokemonLabel.text = "No, es un \(pokemonName)"
+        pokemonImage.kf.setImage(with: URL(string: pokemonImageURL))
+        
     }
     
-
+    @IBAction func playAgainPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
