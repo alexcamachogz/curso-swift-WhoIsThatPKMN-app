f6e6054a0966b491d2483171ddee52d15f9bbe44
Alex Camacho <camacho.ale94@gmail.com>
2022-08-11 12:29:30 -0500

    Clase 23: Flujo de storyboards

---
+++ who is that pokemon/Controller/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsViewController.swift
+//  who is that pokemon
+//
+//  Created by Alex Camacho on 11/08/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
